82c54b7f38e2954b4535eac45366060856529867
joao mario <jjsp0403@gmail.com>
2020-03-02 20:14:34 -0300

    adicionando projeto

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Que braseiro, que fornalha");
+            Console.WriteLine("Nem um pé de plantação");
+            Console.WriteLine("Por falta d'água perdi meu gado");
+            Console.WriteLine("Morreu de sede meu alazão");
+            Console.WriteLine("");
+            Console.WriteLine("Por farta d'água perdi meu gado");
+            Console.WriteLine("Morreu de sede meu alazão");
+            Console.WriteLine("");
+            Console.WriteLine("Até mesmo a asa branca");
+            Console.WriteLine("Bateu asas do sertão");
+            Console.WriteLine("Entonce eu disse, adeus Rosinha");
+            Console.WriteLine("Guarda contigo meu coração");
+        }
+    }
+}
